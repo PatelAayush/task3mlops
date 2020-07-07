dfdf831d90537d9741b16af5d69d337b19ee03a6
Aayush Patel <49674833+PatelAayush@users.noreply.github.com>
2020-07-07 15:35:50 +0530

    Update ml-code.py

--- ml-code.py
+++ ml-code.py
@@ -1,54 +1,113 @@
-from keras.datasets import cifar10
+import keras, sys
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import Flatten
-from keras.constraints import maxnorm
-from keras.optimizers import SGD
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.datasets import mnist
 from keras.utils import np_utils
-# load data
-(X_train, y_train), (X_test, y_test) = cifar10.load_data()
-# normalize inputs from 0-255 to 0.0-1.0
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train = X_train / 255.0
-X_test = X_test / 255.0
-# one hot encode outputs
+
+# Making Command line arguments optional
+# Tweeking Model 
+ker_size = 2
+batch_size_passed = 1024
+no_of_epochs = 1
+crp_count = 1
+fc_count = 1
+if len(sys.argv) == 2:
+    ker_size = int(sys.argv[1])
+elif len(sys.argv) == 3:
+    ker_size = int(sys.argv[1])
+    batch_size_passed = int(sys.argv[2])
+elif len(sys.argv) == 4:
+    ker_size = int(sys.argv[1])
+    batch_size_passed = int(sys.argv[2])
+    no_of_epochs = int(sys.argv[3])
+elif len(sys.argv) == 5:
+    ker_size = int(sys.argv[1])
+    batch_size_passed = int(sys.argv[2])
+    no_of_epochs = int(sys.argv[3])
+    crp_count = int(sys.argv[4])
+elif len(sys.argv) == 6:
+    ker_size = int(sys.argv[1])
+    batch_size_passed = int(sys.argv[2])
+    no_of_epochs = int(sys.argv[3])
+    crp_count = int(sys.argv[4])
+    fc_count = int(sys.argv[5])
+
+# Loading MNIST Dataset
+(x_train, y_train), (x_test, y_test)  = mnist.load_data()
+
+# Finding No. of Rows and Columns
+rows_of_img = x_train[0].shape[0]
+cols_of_img = x_train[1].shape[0]
+
+# store the shape of a single image 
+input_shape = (rows_of_img, cols_of_img, 1)
+
+# change our image type to float32 data type
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Featuring Scaling - Normalization
+x_train /= 255
+x_test /= 255
+
+# Doing One-Hot Encoding
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
-# Create the model
+
+n_classes = y_test.shape[1]
+
+# Set Kernel Size
+kernel_size = (ker_size,ker_size)
+
+# Creating model
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D())
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(Dropout(0.2))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D())
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(Dropout(0.2))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D())
+
+# Adding CRP layers
+model.add(Conv2D(20,kernel_size,padding="same",input_shape=input_shape))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+count = 1
+while count <= crp_count:
+    model.add(Conv2D(50,kernel_size,padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    count+=1
+    
+# FC
 model.add(Flatten())
-model.add(Dropout(0.2))
-model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
-model.add(Dropout(0.2))
-model.add(Dense(num_classes, activation='softmax'))
-# Compile model
-epochs = 25
-lrate = 0.01
-decay = lrate/epochs
-sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-model.summary()
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+
+count = 1
+while count <= fc_count:
+    model.add(Dense(500))
+    model.add(Activation("relu"))
+    count+=1
+    
+model.add(Dense(n_classes))
+model.add(Activation("softmax")) 
+
+model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+
+print(model.summary())
+
+# Conerting Images to 4D
+x_train = x_train.reshape(x_train.shape[0], rows_of_img, cols_of_img, 1)
+x_test = x_test.reshape(x_test.shape[0], rows_of_img, cols_of_img, 1)
+
+# Training Parameters
+batch_size = batch_size_passed
+epochs = no_of_epochs
+
+history = model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test), 
+          shuffle=True)
+
+model.save("mnist_LeNet.h5")   
+
+# Evaluating the accuracy
+scores = model.evaluate(x_test, y_test, verbose=1) 
+print("\nAccuracy is :-\n") 
+print(int(scores[1] * 100)) 
