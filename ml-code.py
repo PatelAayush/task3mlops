d64e06469df7b477658b139cd5d23f88bc799c45
Aayush Patel <49674833+PatelAayush@users.noreply.github.com>
2020-07-09 13:05:36 +0530

    Update ml-code.py

--- ml-code.py
+++ ml-code.py
@@ -2,16 +2,10 @@ from keras.datasets import mnist
 dataset=mnist.load_data()
 
 
-# In[ ]:
-
-
 #Splitting the dataset into appropriate variables
 train, test = dataset
 X_train, y_train = train
 X_test, y_test = test
-
-
-# In[ ]:
 
 
 #Let us reshape the images
@@ -21,15 +15,9 @@ X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
 
-# In[ ]:
-
-
 #Performing one-hot encoding to convert categorical variables into dummy
 from keras.utils.np_utils import to_categorical
 y_train = to_categorical(y_train)
-
-
-# In[ ]:
 
 
 from keras.models import Sequential
@@ -48,13 +36,8 @@ model.add(Dense(units=10, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-# In[ ]:
-
-
 model.fit(X_train, y_train, epochs=1)
 
-
-# In[ ]:
 
 
 a=model.history.history.get('accuracy')
@@ -62,13 +45,9 @@ a= a[0] * 100
 a=int(a)
 
 
-# In[ ]:
-
 
 print("Accurcay of the model is:", a)
 
-
-# In[ ]:
 
 
 f = open("accuracy.txt", "w+")
